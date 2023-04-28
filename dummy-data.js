8545c8142af88fa11fbffae3926046cacc3b5b74
dopamine-s <motor55005@gmail.com>
2023-04-28 03:55:22 +0200

    implementing Featured posts

---
+++ dummy-data.js
@@ -0,0 +1,34 @@
+export const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-with-nextjs.png',
+    excerpt:
+      'Next.js is a JavaScript framework that uses React to build Server Side Render applications (SSR) and statically generated sites. It provides additional structure, features, and optimizations for your application.',
+    date: '2023-04-28',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-with-nextjs.png',
+    excerpt:
+      'Next.js is a JavaScript framework that uses React to build Server Side Render applications (SSR) and statically generated sites. It provides additional structure, features, and optimizations for your application.',
+    date: '2023-04-28',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-with-nextjs.png',
+    excerpt:
+      'Next.js is a JavaScript framework that uses React to build Server Side Render applications (SSR) and statically generated sites. It provides additional structure, features, and optimizations for your application.',
+    date: '2023-04-28',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-with-nextjs.png',
+    excerpt:
+      'Next.js is a JavaScript framework that uses React to build Server Side Render applications (SSR) and statically generated sites. It provides additional structure, features, and optimizations for your application.',
+    date: '2023-04-28',
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import { Fragment } from 'react';
 
 import Hero from '@/components/home-page/hero';
 import FeaturedPosts from '@/components/home-page/featured-posts';
+import { DUMMY_POSTS } from '@/dummy-data';
+
+const posts = DUMMY_POSTS;
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={posts} />
     </Fragment>
   );
 }
