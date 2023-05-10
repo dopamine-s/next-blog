98c4c98bbf7b544c6d52f11ca80674e22176840b
dopamine-s <motor55005@gmail.com>
2023-05-10 08:30:42 +0200

    storing messages with MongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,12 @@
-function handler(req, res) {
+import {
+  createMongoClient,
+  insertDocumentIntoCollection,
+} from '@/helpers/db-utils';
+
+const handlersUri = process.env.NEXT_PUBLIC_API_HANDLERS_URI;
+const mongoClient = createMongoClient(handlersUri);
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -14,12 +22,25 @@ function handler(req, res) {
       return;
     }
 
-    const newMessage = { email, name, message };
-    console.log(newMessage);
+    try {
+      await insertDocumentIntoCollection(mongoClient, 'messages', {
+        email,
+        name,
+        message,
+      });
 
-    res
-      .status(201)
-      .json({ message: 'Successfully stored message!', message: newMessage });
+      const newContact = { email, name, message };
+      console.log(newContact);
+
+      res
+        .status(201)
+        .json({ message: 'Successfully stored message!', contact: newContact });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error! Try again!' });
+    } finally {
+      await mongoClient.close();
+    }
   }
 }
 
